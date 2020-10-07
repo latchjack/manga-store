97f85e7ac92b957b81ccc420432cabd7f4a9505c
Latch Jack <latch.jack@gmail.com>
2020-10-07 19:43:06 +0100

    push

--- config/router.js
+++ config/router.js
@@ -1,21 +1,21 @@
 const router = require('express').Router()
 const mangas = require('../controllers/mangas')
-// const users = require('../controllers/auth')
-// const secureRoute = require('../lib/secureRoute')
+const users = require('../controllers/auth')
+const secureRoute = require('../lib/secureRoute')
 
 router.route('/mangas')
   .get(mangas.index)
-  // .post(secureRoute, mangas.create)
+  .post(secureRoute, mangas.create)
 
 router.route('/mangas/:id')
   .get(mangas.show)
-  // .put(secureRoute, mangas.update)
-  // .delete(secureRoute, mangas.retire)
+  .put(secureRoute, mangas.update)
+  .delete(secureRoute, mangas.retire)
 
 router.route('/register')
-// .post(users.register)
+  .post(users.register)
 
 router.route('/login')
-// .post(users.login)
+  .post(users.login)
 
 module.exports = router
